079ab866053ed82cf5b964a2c4fe63185522cab4
andriy-lun <lvivinbox@gmail.com>
2021-11-05 14:22:06 +0200

    10 objects

--- object.js
+++ object.js
@@ -2,8 +2,8 @@ var num1 = {
     sign: 1,
     nName: "один",
 }
-num1.nName = "one";
-num1.even = false;
+num1.nName = "one"; // rewrite value of the key(property) 
+num1.even = false;  // add new key with boolean value
 console.log(num1);
 
 var num2 = {
@@ -77,3 +77,74 @@ var num10 = {
 num10.nName = "ten";
 num10.even = true;
 console.log(num10);
+
+if (num10.nName === "ten" && num10.sign === 10) { // condition for changing values of last object
+    num10.nName = num10.nName.toUpperCase();
+    num10.sign = "10";
+} else {};
+console.log("\nnew changes added:\n");
+console.log(num10);
+
+num10.even === true                             // condition for remoove and add new values of last object
+    ? (delete num10.even, num10.odd = false)
+    : console.log(num10.even);
+console.log(num10);
+
+console.log("\n Input № of OBJECT")
+const number = 7
+switch (number) {                               //condition SWITCH
+    case 1: {
+        console.log (num1);
+    }
+    break;
+    
+    case 2: {
+        console.log (num2);
+    }
+    break;
+
+    case 3: {
+        console.log (num3);
+    }
+    break;
+
+    case 4: {
+        console.log (num4);
+    }
+    break;
+    
+    case 5: {
+        console.log (num5);
+    }
+    break;
+    
+    case 6: {
+        console.log (num6);
+    }
+    break;
+
+    case 7: {
+        console.log (num7);
+    }
+    break;
+    
+    case 8: {
+        console.log (num8);
+    }
+    break;
+    
+    case 9: {
+        console.log (num9);
+    }
+    break;
+    
+    case 10: {
+        console.log (num10);
+    }
+    break;
+    
+    default: {
+        console.error("Please input OBJECT № from 1 to 10");
+    }
+}
+console.log ("\nHave a nice day!\n")
